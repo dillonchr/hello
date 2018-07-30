6d0af74d3b53556d745a895489bf8a92d23ffeb4
Dillon Christensen <dillon.christensen@kalibrate.com>
2018-07-30 15:52:31 -0500

    patched newnginx script

--- newnginx.sh
+++ newnginx.sh
@@ -3,10 +3,11 @@ if [ "$#" -eq "3" ]; then
     type="$1"
     domain="$2"
     options="$3"
+    conf_path="/etc/nginx/sites-available/${domain}.conf"
 
     if [ "$type" -eq "1" ]; then
     
-    template="
+			cat - > $conf_path <<TEMPL
 server {
   listen 80;
   server_name $domain;
@@ -14,10 +15,10 @@ server {
   location / {
     proxy_pass http://localhost:$options;
     proxy_http_version 1.1;
-    proxy_set_header Upgrade $http_upgrade;
+    proxy_set_header Upgrade \$http_upgrade;
     proxy_set_header Connection 'upgrade';
-    proxy_set_header Host $host;
-    proxy_cache_bypass $http_upgrade;
+    proxy_set_header Host \$host;
+    proxy_cache_bypass \$http_upgrade;
   }
 
   location /.well-known {
@@ -25,11 +26,12 @@ server {
     allow all;
   }
 }
-"
+TEMPL
+
 
     else
         # static
-    template="
+			cat - > $conf_path <<TEMPL
 server {
   listen 80;
 
@@ -38,22 +40,26 @@ server {
   root $options;
 
   location / {
-    try_files $uri $uri/ =404;
+    try_files \$uri \$uri/ =404;
   }
 
   location ~ /.well-known {
     allow all;
   }
 }
-"
+TEMPL
 
     fi
 
-    conf_path="/etc/nginx/sites-available/${domain}.conf"
-    echo $template > $conf_path
-    ln -s $conf_path /etc/nginx/sites-enabled/
-    systemctl restart nginx
-    echo "Added ${domain}"
+    nginx -t
+    if [ "$?" -ne "0" ]; then
+	    rm $conf_path
+	    echo "Failed to add ${domain}"
+    else
+	    ln -s $conf_path /etc/nginx/sites-enabled/
+	    systemctl restart nginx
+	    echo "Added ${domain}"
+    fi
 
 else
     echo "Requires 3 parameters [type (1 proxy, 2 static), domain, if type=1 ? port proxied : path/to/static/files]"
