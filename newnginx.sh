ec394b2ab2dd865618a5c44bad2647c7d8057f1d
Dillon Christensen <j.dillon.christensen@gmail.com>
2018-08-06 21:51:29 -0500

    updated newnginx

--- newnginx.sh
+++ newnginx.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-if [ "$#" -eq "3" ]; then
-type="$1"
-domain="$2"
-options="$3"
+if [ "$#" -ne "2" ]; then
+    echo "Requires 2 parameters [domain, port proxied]"
+    exit 2
+fi
+
+domain="$1"
+port="$2"
 conf_path="/etc/nginx/sites-available/${domain}.conf"
 
+# If path already exists, verify you want to redo it
 if [ -f "$conf_path" ]; then
     echo "${domain} has already been setup."
     read -p "Do you want to remove its configs and run new anyway? [yn]" answer
@@ -16,15 +20,14 @@ if [ -f "$conf_path" ]; then
     esac
 fi
 
-if [ "$type" -eq "1" ]; then
-
-    cat - > $conf_path <<TEMPL
+# add base config to get port 80 wired up
+cat - > $conf_path <<TEMPL
 server {
     listen 80;
     server_name $domain;
 
     location / {
-        proxy_pass http://localhost:$options;
+        proxy_pass http://localhost:$port;
         proxy_http_version 1.1;
         proxy_set_header Upgrade \$http_upgrade;
         proxy_set_header Connection 'upgrade';
@@ -32,65 +35,73 @@ server {
         proxy_cache_bypass \$http_upgrade;
     }
 
-    location /.well-known {
+    location ~ /.well-known {
         alias /var/www/$domain/.well-known;
         allow all;
     }
 }
 TEMPL
-
-
-else
-# static
-    cat - > $conf_path <<TEMPL
-server {
-    listen 80;
-    server_name $domain;
-
-    return 301 https://$server_name$request_uri;
-}
-
-server {
-    listen 443 ssl $domain;
-    listen [::]:443 ssl $domain;
-    ssl_certificate /etc/letsencrypt/live/$domain/fullchain.pem;
-    ssl_certificate_key /etc/letsencrypt/live/$domain/privkey.pem;
-    include snippets/ssl-params.conf;
-    server_name $domain;
-    
-
-    location / {
-        proxy_pass http://localhost:$options;
-        proxy_http_version 1.1;
-        proxy_set_header Upgrade \$http_upgrade;
-        proxy_set_header Connection 'upgrade';
-        proxy_set_header Host \$host;
-        proxy_cache_bypass \$http_upgrade;
-    }
-
-    location /.well-known {
-        alias /var/www/$domain/.well-known;
-        allow all;
-    }
-}
-TEMPL
-
-fi
 
 nginx -t
 
 if [ "$?" -ne "0" ]; then
     rm $conf_path
     echo "Failed to add ${domain}"
-else
-    ln -s $conf_path /etc/nginx/sites-enabled/
-    systemctl restart nginx
-    echo "Added ${domain}"
+    exit 1
 fi
 
-else
-    echo "Requires 3 parameters [type (1 proxy, 2 static), domain, if type=1 ? port proxied : path/to/static/files]"
+# seems to have added it successfully
+ln -s $conf_path /etc/nginx/sites-enabled/
+systemctl restart nginx
+mkdir -p /var/www/$domain 
+certbot certonly -a webroot --webroot-path=/var/www/$domain -d $domain
+
+if [ "$?" -ne "0" ]; then
+    echo "Failed to generate certs. Figure something out"
+    exit 1
 fi
+
+# certs generated fine, lets build the configs to forward 80->443
+cat - > $conf_path <<TEMPL
+server {
+    listen 80;
+    server_name $domain;
+    return https://\$server_name\$request_uri;
+}
+
+server {
+    listen 443 ssl;
+    server_name $domain;
+
+    ssl_certificate /etc/letsencrypt/live/$domain/fullchain.pem;
+    ssl_certificate_key /etc/letsencrypt/live/$domain/privkey.pem;
+    include snippets/ssl-params.conf;
+
+    location / {
+        proxy_pass http://localhost:$port;
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade \$http_upgrade;
+        proxy_set_header Connection 'upgrade';
+        proxy_set_header Host \$host;
+        proxy_cache_bypass \$http_upgrade;
+    }
+
+    location ~ /.well-known {
+        alias /var/www/$domain/.well-known;
+        allow all;
+    }
+}
+TEMPL
+
+nginx -t
+
+if [ "$?" -ne "0" ]; then
+    rm $conf_path
+    echo "Failed to add ${domain} ssl version"
+    exit 1
+fi
+
+systemctl restart nginx
+echo "Added ${domain} successfully!"
 
 echo ""
-
