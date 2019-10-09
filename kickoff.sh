0cdd623572f39e0764ea0cd20638ded1abf771f9
Dillon Christensen <dillon@goloansnap.com>
2019-10-09 15:14:15 -0500

    prepared for PR

--- kickoff.sh
+++ kickoff.sh
@@ -2,7 +2,7 @@ apt update
 apt install -y git
 mkdir /git
 cd /git
-git clone -b features/zsh https://github.com/dillonchr/hello.git
+git clone https://github.com/dillonchr/hello.git
 cd /git/hello/
 chmod +x hello.sh
 ./hello.sh
